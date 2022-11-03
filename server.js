9855b6136def7638b32c760d867f4d4b8ce65962
Sydnei Davis <sydnei.dev@gmail.com>
2022-11-03 19:57:43 -0400

    initial commit: functionality

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+const url = "mongodb+srv://savage:demo@cluster0.uk5wg54.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "savagedemo";
 
 app.listen(3000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
@@ -16,7 +16,7 @@ app.listen(3000, () => {
         db = client.db(dbName);
         console.log("Connected to `" + dbName + "`!");
     });
-});
+})
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -52,7 +52,6 @@ app.put('/messages', (req, res) => {
     res.send(result)
   })
 })
-
 app.delete('/messages', (req, res) => {
   db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
     if (err) return res.send(500, err)
